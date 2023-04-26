6c2edd221c081bf758194262808616a50476cc9c
Timothy A. DeWees <timothy.dewees@gmail.com>
2023-04-25 21:27:03 -0400

    Basic get functionality for rounds to demonstrate concept.  Will work on bows and adding/editing bows next

--- scorecard-app/src/App.js
+++ scorecard-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+import RoundList from './RoundList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Profile() {
+  return <h2>Profile</h2>;
+}
+
+function Home() {
+  return <RoundList></RoundList>;
 }
 
 export default App;
